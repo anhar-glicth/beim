375c3a62d17beb7eeb8703072b5fd594f1549283
anhar-glicth <anhar@example.com>
2026-06-26 17:12:16 +0800

    feat: create HeroHome component with landing page header and call-to-action sections

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import PageIllustration from "@/components/page-illustration";
-import Avatar01 from "@/public/images/avatar-01.jpg";
+import Avatar01 from "@/public/images/avatar-01.jpeg";
 import Avatar02 from "@/public/images/avatar-02.jpg";
 import Avatar03 from "@/public/images/avatar-03.jpg";
 import Avatar04 from "@/public/images/avatar-04.jpg";
